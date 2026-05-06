ecf799907cde600e4ab4c7a11750404319303d95
haly1911 <haly991119@gmail.com>
2026-05-06 22:26:46 +0700

    complete CRUD logic, fetch API and basic product UI

---
+++ src/js/admin/core.js
@@ -0,0 +1,60 @@
+export const API_URL = "https://69cfbef7a4647a9fc675e9d2.mockapi.io/phone/phone";
+
+export const state = {
+  productList: [],
+  editingProduct: null,
+};
+
+export const el = {
+  // dark mode switcher
+  darkModeBtn: document.getElementById("theme-toggle"),
+  // render product list
+  loadingTarget: document.querySelectorAll(".loading-target"),
+  productTableList: document.getElementById("product-table-list"),
+  // dashboard
+  totalTypes: document.getElementById("total-types"),
+  totalQuantity: document.getElementById("total-quantity"),
+  maxPriceValue: document.getElementById("max-price-value"),
+  maxPriceName: document.getElementById("max-price-name"),
+  // search box
+  searchInput: document.getElementById("search-input"),
+  searchBtn: document.getElementById("search-btn"),
+  // sort btn
+  sortUp: document.querySelectorAll(".sort-up"),
+  sortDown: document.querySelectorAll(".sort-down"),
+  // modal
+  openAddModalBtn: document.getElementById("add-new-product"),
+  modal: document.getElementById("modal"),
+  modalOverlay: document.getElementById("modal-overlay"),
+  modalTitle: document.getElementById("modal-title"),
+  form: document.getElementById("product-form"),
+  name: document.getElementById("product-name"),
+  type: document.getElementById("product-type"),
+  quantity: document.getElementById("product-quantity"),
+  plusQuantity: document.getElementById("plus-quantity"),
+  minusQuantity: document.getElementById("minus-quantity"),
+  price: document.getElementById("product-price"),
+  screen: document.getElementById("product-screen"),
+  backCamera: document.getElementById("product-back-camera"),
+  frontCamera: document.getElementById("product-front-camera"),
+  img: document.getElementById("product-img"),
+  imgPreview: document.getElementById("img-preview"),
+  desc: document.getElementById("product-desc"),
+  addBtn: document.getElementById("add-btn"),
+  updateBtn: document.getElementById("update-btn"),
+  resetBtn: document.getElementById("reset-btn"),
+  cancelBtn: document.getElementById("cancel-btn"),
+};
+
+export const errorEl = {
+  spans: document.querySelectorAll(".err-msg"),
+  name: document.getElementById("name-error"),
+  type: document.getElementById("type-error"),
+  quantity: document.getElementById("quantity-error"),
+  price: document.getElementById("price-error"),
+  screen: document.getElementById("screen-error"),
+  backCamera: document.getElementById("back-camera-error"),
+  frontCamera: document.getElementById("front-camera-error"),
+  img: document.getElementById("img-error"),
+  desc: document.getElementById("desc-error"),
+};
